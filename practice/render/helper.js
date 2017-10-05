5ffe79fe593ae8a860a42ed10b6a2f815aaf728f
dhwty <lobanov.igor@gmail.com>
2017-10-06 00:50:47 +0300

    * render/webGL: separate class for water render

--- practice/render/helper.js
+++ practice/render/helper.js
@@ -33,7 +33,7 @@ function getShader(gl, id) {
     return shader;
 }
 
-function handleLoadedTexture(gl,texture) {
+function handleLoadedTexture(gl, texture) {
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
